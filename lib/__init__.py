cf806e7a3433da5dc42bdbe255d2ae761a04afa0
Christos Tzelepis <null.geppetto@gmail.com>
2021-07-26 19:22:45 +0100

    Update

--- lib/__init__.py
+++ lib/__init__.py
@@ -4,10 +4,10 @@ from .config import GAN_RESOLUTIONS, GAN_WEIGHTS, RECONSTRUCTOR_TYPES, BIGGAN_CL
 from .support_sets import SupportSets
 from .reconstructor import Reconstructor
 from .trainer import Trainer
-from .data import PathImages
-from .evaluation.archface.arcface import IDComparator
-from .evaluation.hopenet.hopenet import Hopenet
-from .evaluation.sfd.sfd_detector import SFDDetector
-from .evaluation.fan.fan_model.models import FAN
-from .evaluation.fan.fan_model.utils import find_landmarks
-from .evaluation.fanet.fanet import FANet
+# from .data import PathImages
+# from .evaluation.archface.arcface import IDComparator
+# from .evaluation.hopenet.hopenet import Hopenet
+# from .evaluation.sfd.sfd_detector import SFDDetector
+# from .evaluation.fan.fan_model.models import FAN
+# from .evaluation.fan.fan_model.utils import find_landmarks
+# from .evaluation.fanet.fanet import FANet
