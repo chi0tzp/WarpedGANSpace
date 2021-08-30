800f02917a1d743587a107e8416dc8708ca19b17
Christos Tzelepis <null.geppetto@gmail.com>
2021-08-30 23:07:20 +0300

    Update ProgGAN training script

--- scripts/train/proggan.sh
+++ scripts/train/proggan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GPUS="0"
+GPUS="1,2"
 
 # === Experiment configuration ===
 gan_type="ProgGAN"
@@ -8,8 +8,8 @@ learn_alphas=false
 learn_gammas=true
 num_support_sets=128
 num_support_dipoles=32
-min_shift_magnitude=0.15
-max_shift_magnitude=0.25
+eps=0.1
+max_steps=10
 reconstructor_type="ResNet"
 batch_size=8
 max_iter=200000
@@ -39,8 +39,8 @@ CUDA_VISIBLE_DEVICES=${GPUS} python train.py $tb \
                                              $learn_g \
                                              --num-support-sets=${num_support_sets} \
                                              --num-support-dipoles=${num_support_dipoles} \
-                                             --min-shift-magnitude=${min_shift_magnitude} \
-                                             --max-shift-magnitude=${max_shift_magnitude} \
+                                             --eps=${eps} \
+                                             --max-steps=${max_steps} \
                                              --batch-size=${batch_size} \
                                              --max-iter=${max_iter} \
                                              --log-freq=10 \
