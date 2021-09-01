1af081fee576fd94f3dae2dbed1ca501d498ddb9
Christos Tzelepis <null.geppetto@gmail.com>
2021-09-02 00:02:44 +0300

    Update SNGAN_AnimeFaces training script

--- scripts/train/anime.sh
+++ scripts/train/anime.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GPUS="1"
+GPUS="0,1"
 
 # === Experiment configuration ===
 gan_type="SNGAN_AnimeFaces"
@@ -8,11 +8,11 @@ learn_alphas=false
 learn_gammas=true
 num_support_sets=64
 num_support_dipoles=16
-min_shift_magnitude=0.15
-max_shift_magnitude=0.25
+eps=0.1
+max_steps=10
 reconstructor_type="LeNet"
 batch_size=128
-max_iter=200000
+max_iter=150000
 tensorboard=true
 
 
@@ -39,8 +39,8 @@ CUDA_VISIBLE_DEVICES=${GPUS} python train.py $tb \
                                              $learn_g \
                                              --num-support-sets=${num_support_sets} \
                                              --num-support-dipoles=${num_support_dipoles} \
-                                             --min-shift-magnitude=${min_shift_magnitude} \
-                                             --max-shift-magnitude=${max_shift_magnitude} \
+                                             --eps=${eps} \
+                                             --max-steps=${max_steps} \
                                              --batch-size=${batch_size} \
                                              --max-iter=${max_iter} \
                                              --log-freq=10 \
