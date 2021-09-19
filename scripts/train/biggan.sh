fb4610ca465895eac8e0e598bb013ce593f768fc
Christos Tzelepis <null.geppetto@gmail.com>
2021-09-20 02:37:27 +0300

    Refactor progan, stylegan2 aux training scripts

--- scripts/train/biggan.sh
+++ scripts/train/biggan.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-GPUS="0,1"
 
+# === Experiment configuration ===
 gan_type="BigGAN"
 biggan_target_classes="239"
 learn_alphas=false
 learn_gammas=true
 num_support_sets=120
-num_support_dipoles=512
+num_support_dipoles=256
 min_shift_magnitude=0.15
 max_shift_magnitude=0.25
 reconstructor_type="ResNet"
 batch_size=32
 max_iter=200000
 tensorboard=true
+# ================================
 
 
-# === Run training script ===
+# Run training script
 learn_a=""
 if $learn_alphas ; then
   learn_a="--learn-alphas"
@@ -32,18 +33,17 @@ if $tensorboard ; then
   tb="--tensorboard"
 fi
 
-CUDA_VISIBLE_DEVICES=${GPUS} python train.py $tb \
-                                             --multi-gpu \
-                                             --gan-type=${gan_type} \
-                                             --biggan-target-classes=${biggan_target_classes} \
-                                             --reconstructor-type=${reconstructor_type} \
-                                             $learn_a \
-                                             $learn_g \
-                                             --num-support-sets=${num_support_sets} \
-                                             --num-support-dipoles=${num_support_dipoles} \
-                                             --min-shift-magnitude=${min_shift_magnitude} \
-                                             --max-shift-magnitude=${max_shift_magnitude} \
-                                             --batch-size=${batch_size} \
-                                             --max-iter=${max_iter} \
-                                             --log-freq=10 \
-                                             --ckp-freq=100
+python train.py $tb \
+                --gan-type=${gan_type} \
+                --biggan-target-classes=${biggan_target_classes} \
+                --reconstructor-type=${reconstructor_type} \
+                $learn_a \
+                $learn_g \
+                --num-support-sets=${num_support_sets} \
+                --num-support-dipoles=${num_support_dipoles} \
+                --min-shift-magnitude=${min_shift_magnitude} \
+                --max-shift-magnitude=${max_shift_magnitude} \
+                --batch-size=${batch_size} \
+                --max-iter=${max_iter} \
+                --log-freq=10 \
+                --ckp-freq=100
--- scripts/train/proggan.sh
+++ scripts/train/proggan.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-GPUS="0"
 
 # === Experiment configuration ===
 gan_type="ProgGAN"
 learn_alphas=false
 learn_gammas=true
-num_support_sets=128
-num_support_dipoles=32
+num_support_sets=200
+num_support_dipoles=512
 min_shift_magnitude=0.15
 max_shift_magnitude=0.25
 reconstructor_type="ResNet"
 batch_size=8
 max_iter=200000
 tensorboard=true
+# ================================
 
 
-# === Run training script ===
+# Run training script
 learn_a=""
 if $learn_alphas ; then
   learn_a="--learn-alphas"
@@ -32,16 +32,16 @@ if $tensorboard ; then
   tb="--tensorboard"
 fi
 
-CUDA_VISIBLE_DEVICES=${GPUS} python train.py $tb \
-                                             --gan-type=${gan_type} \
-                                             --reconstructor-type=${reconstructor_type} \
-                                             $learn_a \
-                                             $learn_g \
-                                             --num-support-sets=${num_support_sets} \
-                                             --num-support-dipoles=${num_support_dipoles} \
-                                             --min-shift-magnitude=${min_shift_magnitude} \
-                                             --max-shift-magnitude=${max_shift_magnitude} \
-                                             --batch-size=${batch_size} \
-                                             --max-iter=${max_iter} \
-                                             --log-freq=10 \
-                                             --ckp-freq=100
+python train.py $tb \
+                --gan-type=${gan_type} \
+                --reconstructor-type=${reconstructor_type} \
+                $learn_a \
+                $learn_g \
+                --num-support-sets=${num_support_sets} \
+                --num-support-dipoles=${num_support_dipoles} \
+                --min-shift-magnitude=${min_shift_magnitude} \
+                --max-shift-magnitude=${max_shift_magnitude} \
+                --batch-size=${batch_size} \
+                --max-iter=${max_iter} \
+                --log-freq=10 \
+                --ckp-freq=100
