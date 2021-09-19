dd4ab96fa758971a6ccd161b422dd9c3edfce441
Christos Tzelepis <null.geppetto@gmail.com>
2021-09-20 02:29:56 +0300

    Refactor mnist, animefaces aux training scripts

--- scripts/train/anime.sh
+++ scripts/train/anime.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-GPUS="1"
 
 # === Experiment configuration ===
 gan_type="SNGAN_AnimeFaces"
 learn_alphas=false
 learn_gammas=true
 num_support_sets=64
-num_support_dipoles=16
+num_support_dipoles=128
 min_shift_magnitude=0.15
 max_shift_magnitude=0.25
 reconstructor_type="LeNet"
 batch_size=128
-max_iter=200000
+max_iter=120000
 tensorboard=true
+# ================================
 
 
-# === Run training script ===
+# Run training script
 learn_a=""
 if $learn_alphas ; then
   learn_a="--learn-alphas"
@@ -32,16 +32,16 @@ if $tensorboard ; then
   tb="--tensorboard"
 fi
 
-CUDA_VISIBLE_DEVICES=${GPUS} python train.py $tb \
-                                             --gan-type=${gan_type} \
-                                             --reconstructor-type=${reconstructor_type} \
-                                             $learn_a \
-                                             $learn_g \
-                                             --num-support-sets=${num_support_sets} \
-                                             --num-support-dipoles=${num_support_dipoles} \
-                                             --min-shift-magnitude=${min_shift_magnitude} \
-                                             --max-shift-magnitude=${max_shift_magnitude} \
-                                             --batch-size=${batch_size} \
-                                             --max-iter=${max_iter} \
-                                             --log-freq=10 \
-                                             --ckp-freq=100
+python train.py $tb \
+                --gan-type=${gan_type} \
+                --reconstructor-type=${reconstructor_type} \
+                $learn_a \
+                $learn_g \
+                --num-support-sets=${num_support_sets} \
+                --num-support-dipoles=${num_support_dipoles} \
+                --min-shift-magnitude=${min_shift_magnitude} \
+                --max-shift-magnitude=${max_shift_magnitude} \
+                --batch-size=${batch_size} \
+                --max-iter=${max_iter} \
+                --log-freq=10 \
+                --ckp-freq=100
