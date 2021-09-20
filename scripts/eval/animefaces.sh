21cecc0dd94ab8b7be650c0d5996e26dd33da9a9
Christos Tzelepis <null.geppetto@gmail.com>
2021-09-20 15:27:15 +0300

    Fix animefaces aux evaluation script

--- scripts/eval/animefaces.sh
+++ scripts/eval/animefaces.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
 pool="SNGAN_AnimeFaces_6"
-eps=0.2
-shift_steps=32
+eps=0.25
+shift_steps=24
 
-declare -a EXPERIMENTS=("experiments/complete/SNGAN_AnimeFaces-LeNet-K64-N1-LearnGammas-eps0.15_0.25"
-                        "experiments/complete/SNGAN_AnimeFaces-LeNet-K64-N16-LearnGammas-eps0.15_0.25"
-                        "experiments/complete/SNGAN_AnimeFaces-LeNet-K64-N32-LearnGammas-eps0.15_0.25"
-                        "experiments/complete/SNGAN_AnimeFaces-LeNet-K64-N64-LearnGammas-eps0.15_0.25"
-                        "experiments/complete/SNGAN_AnimeFaces-LeNet-K64-N128-LearnGammas-eps0.15_0.25")
-
+declare -a EXPERIMENTS=("experiments/complete/SNGAN_AnimeFaces-LeNet-K64-D128-LearnGammas-eps0.25_0.35")
 
 for exp in "${EXPERIMENTS[@]}"
 do
   # Traverse latent space
-  python traverse_latent_space.py -v --gif --gif-size=96 --exp="${exp}" --pool=${pool} --eps=${eps} --shift-steps=${shift_steps}
+  python traverse_latent_space.py -v --gif --exp="${exp}" --pool=${pool} --eps=${eps} --shift-steps=${shift_steps}
 done
