71e34d7f29a63399bc6f64593205cca40d4f4dce
Christos Tzelepis <null.geppetto@gmail.com>
2021-09-20 14:38:27 +0300

    Fix mnist aux evaluation script

--- scripts/eval/mnist.sh
+++ scripts/eval/mnist.sh
@@ -4,11 +4,7 @@ pool="SNGAN_MNIST_10"
 eps=0.2
 shift_steps=16
 
-declare -a EXPERIMENTS=("experiments/complete/SNGAN_MNIST-LeNet-K64-N1-LearnGammas-eps0.15_0.25"
-                        "experiments/complete/SNGAN_MNIST-LeNet-K64-N16-LearnGammas-eps0.15_0.25"
-                        "experiments/complete/SNGAN_MNIST-LeNet-K64-N32-LearnGammas-eps0.15_0.25"
-                        "experiments/complete/SNGAN_MNIST-LeNet-K64-N64-LearnGammas-eps0.15_0.25"
-                        "experiments/complete/SNGAN_MNIST-LeNet-K64-N128-LearnGammas-eps0.15_0.25")
+declare -a EXPERIMENTS=("experiments/complete/SNGAN_MNIST-LeNet-K64-D128-LearnGammas-eps0.15_0.25")
 
 for exp in "${EXPERIMENTS[@]}"
 do
