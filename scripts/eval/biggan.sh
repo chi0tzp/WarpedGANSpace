30208a38cdc6675517b368f1822e81c21d5c9381
Christos Tzelepis <null.geppetto@gmail.com>
2021-09-22 14:04:49 +0300

    Fix biggan aux evaluation script

--- scripts/eval/biggan.sh
+++ scripts/eval/biggan.sh
@@ -5,7 +5,7 @@ eps=0.2
 shift_steps=16
 batch_size=8
 
-declare -a EXPERIMENTS=("experiments/complete/BigGAN-239-ResNet-K120-N512-LearnGammas-eps0.15_0.25")
+declare -a EXPERIMENTS=("experiments/complete/BigGAN-239-ResNet-K120-D256-LearnGammas-eps0.15_0.25")
 
 for exp in "${EXPERIMENTS[@]}"
 do
