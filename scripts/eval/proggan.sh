75b9023129f643f6b6d672c3e2e22d5687b0d43e
Christos Tzelepis <null.geppetto@gmail.com>
2021-10-06 00:22:27 +0300

    ProgGAN and StyleGAN2 aux eval scripts

--- scripts/eval/proggan.sh
+++ scripts/eval/proggan.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 
-pool="ProgGAN_6"
-eps=0.2
+pool="ProgGAN_1"
+eps=0.15
 shift_steps=24
-batch_size=4
+shift_leap=5
+batch_size=8
 
-declare -a EXPERIMENTS=("experiments/complete/ProgGAN-ResNet-K128-N1-LearnGammas-eps0.15_0.25"
-                        "experiments/complete/ProgGAN-ResNet-K128-N32-LearnGammas-eps0.15_0.25"
-                        "experiments/complete/ProgGAN-ResNet-K128-N64-LearnGammas-eps0.15_0.25"
-                        "experiments/complete/ProgGAN-ResNet-K128-N128-LearnGammas-eps0.15_0.25"
-                        "experiments/complete/ProgGAN-ResNet-K128-N256-LearnGammas-eps0.15_0.25"
-                        "experiments/complete/ProgGAN-ResNet-K128-N512-LearnGammas-eps0.15_0.25"
-                        "experiments/complete/ProgGAN-ResNet-K200-N512-LearnGammas-eps0.15_0.25")
+declare -a EXPERIMENTS=("experiments/complete/ProgGAN-ResNet-K200-D512-LearnGammas-eps0.1_0.2")
 
 for exp in "${EXPERIMENTS[@]}"
 do
   # Traverse latent space
-  python traverse_latent_space.py -v --gif --exp="${exp}" --pool=${pool} --eps=${eps} --shift-steps=${shift_steps} --batch-size=${batch_size}
+  # python traverse_latent_space.py -v --gif --exp="${exp}" --pool=${pool} --eps=${eps} --shift-steps=${shift_steps} --shift-leap=${shift_leap} --batch-size=${batch_size}
   # Traverse attribute space
   python traverse_attribute_space.py -v --exp="${exp}" --pool=${pool} --eps=${eps} --shift-steps=${shift_steps}
   # Rank interpretable paths
-  python rank_interpretable_paths.py -v --exp="${exp}" --pool=${pool} --eps=${eps} --shift-steps=${shift_steps}
+  # python rank_interpretable_paths_bkp.py -v --exp="${exp}" --pool=${pool} --eps=${eps} --shift-steps=${shift_steps}
 done
--- scripts/eval/stylegan2.sh
+++ scripts/eval/stylegan2.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-pool="StyleGAN2_6"
-eps=0.075
-shift_steps=32
-batch_size=5
+pool="StyleGAN2_2"
+eps=0.15
+shift_steps=7
+batch_size=8
 
 declare -a EXPERIMENTS=("experiments/complete/StyleGAN2-1024-W-ResNet-K200-N512-LearnGammas-eps0.1_0.2")
 
 for exp in "${EXPERIMENTS[@]}"
 do
   # Traverse latent space
-  python traverse_latent_space.py -v --gif --exp="${exp}" --pool=${pool} --eps=${eps} --shift-steps=${shift_steps} --batch-size=${batch_size}
+  # python traverse_latent_space.py -v --gif --exp="${exp}" --pool=${pool} --eps=${eps} --shift-steps=${shift_steps} --batch-size=${batch_size}
   # Traverse attribute space
   python traverse_attribute_space.py -v --exp="${exp}" --pool=${pool} --eps=${eps} --shift-steps=${shift_steps}
   # Rank interpretable paths
-  python rank_interpretable_paths.py -v --exp="${exp}" --pool=${pool} --eps=${eps} --shift-steps=${shift_steps}
+  # python rank_interpretable_paths.py -v --exp="${exp}" --pool=${pool} --eps=${eps} --shift-steps=${shift_steps}
 done
