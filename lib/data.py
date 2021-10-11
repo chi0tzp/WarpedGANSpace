6217774f78766943e09bef073c91e845124769f6
Christos Tzelepis <null.geppetto@gmail.com>
2021-10-11 06:04:00 +0300

    Dataloader returns images in path in their original resolution

--- lib/data.py
+++ lib/data.py
@@ -7,8 +7,7 @@ from torch.utils import data
 
 
 class PathImages(data.Dataset):
-    def __init__(self, root_path, size=256):
-        self.size = size
+    def __init__(self, root_path):
         self.images_files = glob.glob(osp.join(root_path, '*.jpg'))
         self.images_files.sort()
 
@@ -18,10 +17,9 @@ class PathImages(data.Dataset):
     def __getitem__(self, index):
         return self.image2tensor(self.images_files[index])
 
-    def image2tensor(self, image_file):
-        # Open image in BGR order, resize and convert to RBG order
+    @staticmethod
+    def image2tensor(image_file):
+        # Open image in BGR order and convert to RBG order
         img = cv2.imread(image_file, cv2.IMREAD_COLOR)
-        img = cv2.resize(img, (self.size, self.size))
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB).astype('uint8')
-
         return torch.tensor(np.transpose(img, (2, 0, 1))).float()
