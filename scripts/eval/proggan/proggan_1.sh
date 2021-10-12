d5921b7ca4bb0ed79c286512ce62ea4e8f03b642
Christos Tzelepis <null.geppetto@gmail.com>
2021-10-12 21:15:43 +0300

    Remove proggan_1 aux eval script

--- scripts/eval/proggan/proggan_1.sh
+++ scripts/eval/proggan/proggan_1.sh
@@ -3,7 +3,7 @@
 pool="ProgGAN_1"
 eps=0.15
 shift_steps=32
-shift_leap=4
+shift_leap=3
 batch_size=16
 
 declare -a EXPERIMENTS=("experiments/complete/ProgGAN-ResNet-K200-D512-LearnGammas-eps0.1_0.2")
@@ -11,9 +11,9 @@ declare -a EXPERIMENTS=("experiments/complete/ProgGAN-ResNet-K200-D512-LearnGamm
 for exp in "${EXPERIMENTS[@]}"
 do
   # Traverse latent space
-  # python traverse_latent_space.py -v --gif --exp="${exp}" --pool=${pool} --eps=${eps} --shift-steps=${shift_steps} --shift-leap=${shift_leap} --batch-size=${batch_size}
+  python traverse_latent_space.py -v --gif --exp="${exp}" --pool=${pool} --eps=${eps} --shift-steps=${shift_steps} --shift-leap=${shift_leap} --batch-size=${batch_size}
   # Traverse attribute space
-  # python traverse_attribute_space.py -v --exp="${exp}" --pool=${pool} --eps=${eps} --shift-steps=${shift_steps}
+  python traverse_attribute_space.py -v --exp="${exp}" --pool=${pool} --eps=${eps} --shift-steps=${shift_steps}
   # Rank interpretable paths
   python rank_interpretable_paths.py -v --exp="${exp}" --pool=${pool} --eps=${eps} --shift-steps=${shift_steps}
 done
