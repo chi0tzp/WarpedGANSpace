7da75f73abc61c3679a9fedd6d5ad03fda4a6d9e
Christos Tzelepis <null.geppetto@gmail.com>
2021-10-12 07:09:42 +0300

    Add 5 CelebA attributes to the attribute space (from Talk-to-Edit)

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,6 +1,7 @@
 from .aux import create_exp_dir, update_stdout, update_progress, sample_z
 from .config import GAN_RESOLUTIONS, GAN_WEIGHTS, RECONSTRUCTOR_TYPES, BIGGAN_CLASSES, SFD, ARCFACE, FAIRFACE, AUDET, \
-    HOPENET, SNGAN_MNIST_LeNet_K64_D128_LearnGammas_eps0d15_0d25, \
+    HOPENET, CELEBA_ATTRIBUTES, \
+    SNGAN_MNIST_LeNet_K64_D128_LearnGammas_eps0d15_0d25, \
     SNGAN_AnimeFaces_LeNet_K64_D128_LearnGammas_eps0d25_0d35, \
     BigGAN_239_ResNet_K120_D256_LearnGammas_eps0d15_0d25, ProgGAN_ResNet_K200_D512_LearnGammas_eps0d1_0d2
 from .support_sets import SupportSets
@@ -11,3 +12,4 @@ from .evaluation.archface.arcface import IDComparator
 from .evaluation.hopenet.hopenet import Hopenet
 from .evaluation.sfd.sfd_detector import SFDDetector
 from .evaluation.au_detector.AU_detector import AUdetector
+from .evaluation.celeba_attributes.celeba_attr_predictor import celeba_attr_predictor
