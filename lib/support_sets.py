b23de3fb33fd2870032ab3693b7b244cd929be34
Christos Tzelepis <null.geppetto@gmail.com>
2021-12-08 09:43:33 +0000

    Debug grad_f formula

--- lib/support_sets.py
+++ lib/support_sets.py
@@ -118,7 +118,8 @@ class SupportSets(nn.Module):
 
         # REVIEW: Fix `grad_f` formula
         # grad_f = -2 * (alphas_batch * torch.exp(-gammas_batch * (torch.norm(D, dim=2) ** 2).unsqueeze(dim=2)) * D).sum(dim=1)
-        grad_f = -2 * (alphas_batch * gammas_batch * torch.exp(-gammas_batch * (torch.norm(D, dim=2) ** 2).unsqueeze(dim=2)) * D).sum(dim=1)
-        
+        grad_f = -2 * (alphas_batch * gammas_batch *
+                       torch.exp(-gammas_batch * (torch.norm(D, dim=2) ** 2).unsqueeze(dim=2)) * D).sum(dim=1)
+
         # Return normalized grad of f at z
         return grad_f / torch.norm(grad_f, dim=1, keepdim=True)
